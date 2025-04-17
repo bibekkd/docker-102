104564f16c6ac2c36fc8155787fe172270d740e1
Bibek Dey <bibek@Bibeks-MacBook-Air.local>
2025-04-17 20:06:37 +0530

    Initial commit basic code written in typescipt and Dockerfile created

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req: any, res: any) => {
+    res.send('Hello typescript in docker!');
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
